d5277522f8d7f7525460be578be112c03f857988
Syed Arif Iqbal <arifiqbal@outlook.com>
2023-04-14 18:49:46 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TransactionTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
