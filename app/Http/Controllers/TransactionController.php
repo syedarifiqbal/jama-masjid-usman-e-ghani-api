9e38aaf5b5332d93ac2df6a821c5d807ed90ce99
Syed Arif Iqbal <arifiqbal@outlook.com>
2023-04-20 01:57:09 +0500

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -135,7 +135,7 @@ class TransactionController extends Controller
         
         $fcmNotifier = new FCMNotifier();
         $deviceTokens = [$transaction->owner->fcm_token];
-        $title = "Transaction Approved By: {auth()->user()->name}";
+        $title = "Transaction Approved By: " . auth()->user()->name;
         $body = "Amount: {$transaction->amount} \\nDescription: {$transaction->description}";
         $data = ['transaction_id' => $transaction->id];
         $fcmNotifier->notify($deviceTokens, $title, $body, $data);
