2a933059d62957f89ced2d6e1347607b7c8e0810
Syed Arif Iqbal <arifiqbal@outlook.com>
2023-04-20 01:51:05 +0500

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -23,8 +23,12 @@ class TransactionController extends Controller
     {
         return Transaction::with('owner:id,name,email,is_admin')
             ->with('approver:id,name,email,is_admin')
+            ->with('category:id,name')
             ->when($request->from && $request->to, function ($q) use ($request) {
                 $q->whereBetween('transaction_date', [$request->from, $request->to]);
+            })
+            ->when($request->category, function ($q) use ($request) {
+                $q->where('category_id', $request->category);
             })
             ->when($request->type, function ($q) use ($request) {
                 $q->where('transaction_type_id', $request->type);
@@ -52,19 +56,13 @@ class TransactionController extends Controller
         $transaction->save();
 
         try {
-            // Create an instance of FCMNotifier
             $fcmNotifier = new FCMNotifier();
-
-            // Prepare the device tokens, notification title, body, and optional data
-            // $deviceTokens = ['d-eObWbdQ-Oc7hIKRT0qQq:APA91bH5lg1AbdrwooGHff-ICXRSCVHmllJYepzrbt6SJGARxHER1udbwS3EJ-Rixkg70g0XEeBgu0EcyZmVnxg5trIenmVC0uKNAane0tNwbmDnercJlInDiljWxDkOQF-CvWu4IV3p'];
             $deviceTokens = User::query()
-                            // ->whereKeyNot(auth()->id())
+                            ->whereKeyNot(auth()->id())
                             ->where('is_admin', true)->pluck('fcm_token');
             $title = 'New Transactions Created By: ' . auth()->user()->name;
-            $body = 'Amount: ' . $transaction->amount . '\\nDescription: ' . $transaction->description;
+            $body = "Amount: {$transaction->amount} \\nDescription: {$transaction->description}";
             $data = ['transaction_id' => $transaction->id];
-
-            // Send the notification
             $fcmNotifier->notify($deviceTokens, $title, $body, $data);
 
         } catch (\Exception $th) {
@@ -134,6 +132,14 @@ class TransactionController extends Controller
         $transaction->approver()->associate(auth()->user());
         $transaction->approved_at = now()->toDateTimeString();
         $transaction->save();
+        
+        $fcmNotifier = new FCMNotifier();
+        $deviceTokens = [$transaction->owner->fcm_token];
+        $title = "Transaction Approved By: {auth()->user()->name}";
+        $body = "Amount: {$transaction->amount} \\nDescription: {$transaction->description}";
+        $data = ['transaction_id' => $transaction->id];
+        $fcmNotifier->notify($deviceTokens, $title, $body, $data);
+
         return response()->json(['message' => 'Transaction updated successfully!'], Response::HTTP_OK);
     }
 
